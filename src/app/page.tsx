00703b7934f7738eb8c4653cb47f4ea6904e3813
BrendanWaspe <brendanwaspe@gmail.com>
2025-09-18 10:48:32 +0200

    Convex Queries and Mutations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="m-10">
-    <SignInButton>
-      <Button>Login</Button>
-    </SignInButton>
-  </div>
+  return (
+    <div className="m-10">
+      <SignInButton>
+        <Button>Login</Button>
+      </SignInButton>
+    </div>
+  );
 }
